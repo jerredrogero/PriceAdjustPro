40c254608fffbdc9e98a45e9fa7c8b2216e1846b
Jerred Rogero <jrogero578@gamail.com>
2025-06-03 14:17:06 -0400

    using env variables for icloud email

--- price_adjust_pro/price_adjust_pro/settings.py
+++ price_adjust_pro/price_adjust_pro/settings.py
@@ -196,7 +196,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'noreply@priceadjustpro')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'noreply@priceadjustpro'
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'noreply@priceadjustpro')
 EMAIL_TIMEOUT = 30
 
 # SSL settings for email
