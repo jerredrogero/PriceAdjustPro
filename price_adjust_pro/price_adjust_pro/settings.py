536d52e493b59e0d7de23c888b688c420085b16c
Jerred Rogero <jrogero578@gamail.com>
2025-06-03 14:24:11 -0400

    added default from email

--- price_adjust_pro/price_adjust_pro/settings.py
+++ price_adjust_pro/price_adjust_pro/settings.py
@@ -194,9 +194,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.me.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'noreply@priceadjustpro')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'jerredrogero@icloud.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'noreply@priceadjustpro')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@priceadjustpro.com')
 EMAIL_TIMEOUT = 30
 
 # SSL settings for email
